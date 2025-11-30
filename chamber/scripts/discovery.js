2789302ff931c294406c4be47268b1279f106fa6
HarryMichael <harrymichaelasa@gmail.com>
2025-11-30 21:57:59 +0000

    Discover Page

--- chamber/scripts/discovery.js
+++ chamber/scripts/discovery.js
@@ -13,7 +13,7 @@ items.forEach((item, index) => {
     card.innerHTML = `
       <h2>${item.name}</h2>
       <figure>
-        <img src="${item.image}" alt="${item.name}" width="300" height="200">
+        <img src="${item.image}" alt="${item.name}" width="300" height="200" loading="lazy">
       </figure>
       <address>${item.address}</address>
       <p>${item.description}</p>
